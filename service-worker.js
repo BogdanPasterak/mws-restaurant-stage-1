99884c65026ca8b0c951f954c7b81b045b747b87
BogdanPasterak <bogdanpasterak@gmail.com>
2018-08-01 11:19:32 +0100

    feat:Service Worker

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+
+const CACHE_NAME = 'mws-restaurant-stage-cache-001';
+
+// List of files which are store in cache.
+let filesToCache = [
+    '/',
+    '/styles/main.css',
+    '/images/logo.png',
+    '/scripts/main.js'
+];
+
+self.addEventListener('install', function (evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            return cache.addAll(filesToCache);
+        }).catch(function (err) {
+            // Snooze errors...
+            // console.error(err);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (evt) {
+    // Snooze logs...
+    // console.log('Start ' + CACHE_NAME + '/n' + evt.request.url);
+    evt.respondWith(
+        // Firstly, send request..
+        fetch(evt.request).catch(function () {
+            // When request failed, return file from cache...
+            return caches.match(evt.request);
+        })
+    );
+});
+
